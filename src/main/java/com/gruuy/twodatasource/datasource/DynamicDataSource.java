f2c6635f34043108383e4c7b46260623ec884215
吴梓健 <245746119@qq.com>
2019-06-04 17:17:28 +0800

    添加数据源切换注释

--- src/main/java/com/gruuy/twodatasource/datasource/DynamicDataSource.java
+++ src/main/java/com/gruuy/twodatasource/datasource/DynamicDataSource.java
@@ -11,7 +11,9 @@ public class DynamicDataSource extends AbstractRoutingDataSource {
 
     private static final Logger log= LoggerFactory.getLogger(DynamicDataSource.class);
 
-    @Override
+
+    /** 就是这里设置的数据源  不要被Object迷惑了 */
+  @Override
     protected Object determineCurrentLookupKey() {
         logger.debug("数据源为"+DataSourceContextUtils.getDB());
         return DataSourceContextUtils.getDB();
